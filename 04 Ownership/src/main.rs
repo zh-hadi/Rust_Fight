3f94230446306f64a278bad71625f23a28156f2c
Hadiuzzaman Hadi <zhhadi50@gmail.com>
2023-05-27 23:46:25 +0600

    Learn Rust Ownership

---
+++ 04 Ownership/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // ownership 
+    // this mean momory free dynamicly 
+    // Scope outer value delete dynamicly
+
+    // Ownership rouls
+    // -1. Each value in Rust has an owner.
+    // -2. There can only be one owner at a time.
+    // -3. When the owner goes out of scope, the value will be dropped.
+}
