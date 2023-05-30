0e8862d7f085daade00ecc2f22ee2664b3d2b58a
Hadiuzzaman Hadi <zhhadi50@gmail.com>
2023-05-30 19:00:58 +0600

    In Rust how to declear method

--- 06 Stract and Method/src/main.rs
+++ 06 Stract and Method/src/main.rs
@@ -1,14 +1,22 @@
 #[derive(Debug)]
-struct Reactangle {
+struct Rectangle {
     width: u32,
     height: u32,
 }
 
+impl Rectangle {
+    fn area( &self)->u32{
+        self.width * self.height
+    }
+}
+
 fn main() {
-    let rec1 = Reactangle{
+    let rec1 = Rectangle{
         width: 30,
         height: 40,
     };
+
+    println!("The area is Squar Braket {}", rec1.area());
 
     dbg!(rec1);
 }
